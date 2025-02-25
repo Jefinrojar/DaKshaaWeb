3bc6e16b0c6f93bf9e97fa91f4665eae84a359f6
ArulG2005 <mailtogarul@gmail.com>
2025-02-25 19:49:46 +0530

    footer added

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import HeroSection from './Components/HeroSection'
+import Footer from './Components/Footer'
 const Home = () => {
   return (
     <section>
       <HeroSection/>
+      <Footer/>
     </section>
   )
 }
