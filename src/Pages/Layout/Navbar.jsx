c6fd24b942677ef1434b3ba9f686d71ff20c9325
Sharan_505 <sharan27505@gmail.com>
2025-02-25 19:20:32 +0530

    rectified navbar

--- src/Pages/Layout/Navbar.jsx
+++ src/Pages/Layout/Navbar.jsx
@@ -10,71 +10,70 @@ const Navbar = () => {
   };
 
   return (
-    <div className="mx-5 md:mx-20 mt-5">
-      <div className="grid grid-cols-12">
-        <img
-          className="col-span-6 md:col-span-3 w-32 md:w-48"
-          src={logo}
-          alt=""
-        />
+    <div className="fixed top-0 left-0 right-0 z-50">
+        <div className="mx-5 md:mx-20 py-3">
+            <div className="grid grid-cols-12">
+            <img
+                className="col-span-6 md:col-span-3 w-32 md:w-48"
+                src={logo}
+                alt=""
+            />
 
-        {/* Burger Menu Icon */}
-        <div className="col-span-6 md:hidden flex justify-end items-center">
-          <button
-            onClick={toggleMenu}
-            className="text-white focus:outline-none"
-          >
-            <svg
-              className="w-8 h-8"
-              fill="none"
-              stroke="currentColor"
-              viewBox="0 0 24 24"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M4 6h16M4 12h16m-7 6h7"
-              />
-            </svg>
-          </button>
+            {/* Burger Menu Icon */}
+            <div className="col-span-6 md:hidden flex justify-end items-center">
+                <button
+                onClick={toggleMenu}
+                className="text-white focus:outline-none"
+                >
+                <svg
+                    className="w-8 h-8"
+                    fill="none"
+                    stroke="currentColor"
+                    viewBox="0 0 24 24"
+                >
+                    <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M4 6h16M4 12h16m-7 6h7"
+                    />
+                </svg>
+                </button>
+            </div>
+
+            {/* Navigation Links */}
+            <div className='md:col-span-6 col-span-12 mt-4'>
+                <div class="gradient-line animate-blinkOpacity relative"></div>
+                <ul
+                className={`${
+                    isMenuOpen ? "block" : "hidden"
+                } font-poppins md:flex mt-2 text-white justify-center items-center text-xl gap-12`}
+                >
+                <li className="cursor-pointer py-1 rounded-xl hover:underline hover:underline-offset-8">
+                    Home
+                </li>
+                <li className="cursor-pointer py-1 rounded-xl hover:underline hover:underline-offset-8">
+                    About
+                </li>
+                <li className="cursor-pointer py-1 rounded-xl hover:underline hover:underline-offset-8">
+                    Events
+                </li>
+                <li className="cursor-pointer py-1 rounded-xl hover:underline hover:underline-offset-8">
+                    FAQ
+                </li>
+                <li className="cursor-pointer py-1 rounded-xl hover:underline hover:underline-offset-8">
+                    Contact
+                </li>
+                </ul>
+            </div>
+
+            {/* College Logo */}
+            <div className="hidden md:col-span-3 md:flex justify-end items-center">
+                <img className="w-56" src={collegeLogo} alt="" />
+            </div>
+            </div>
         </div>
-
-        {/* Navigation Links */}
-        <ul
-  className={`${
-    isMenuOpen ? "block" : "hidden"
-  } md:col-span-6 md:flex font-orbitron text-white justify-center items-center text-2xl gap-10 col-span-12 mt-4 md:mt-0`}
->
-  <li className='group relative cursor-pointer px-4 py-1 rounded-xl underline underline-offset-8 hover:no-underline'>
-    {/* Top line (overline) that appears on hover */}
-    <span className='absolute inset-x-0 top-0 h-0.5 bg-white transform scale-x-0 transition-transform duration-300 ease-out group-hover:scale-x-100'></span>
-    Home
-  </li>
-  <li className='group relative cursor-pointer px-4 py-1 rounded-xl underline underline-offset-8 hover:no-underline'>
-    <span className='absolute inset-x-0 top-0 h-0.5 bg-white transform scale-x-0 transition-transform duration-300 ease-out group-hover:scale-x-100'></span>
-    About
-  </li>
-  <li className='group relative cursor-pointer px-4 py-1 rounded-xl underline underline-offset-8 hover:no-underline'>
-    <span className='absolute inset-x-0 top-0 h-0.5 bg-white transform scale-x-0 transition-transform duration-300 ease-out group-hover:scale-x-100'></span>
-    Events
-  </li>
-  <li className='group relative cursor-pointer px-4 py-1 rounded-xl underline underline-offset-8 hover:no-underline'>
-    <span className='absolute inset-x-0 top-0 h-0.5 bg-white transform scale-x-0 transition-transform duration-300 ease-out group-hover:scale-x-100'></span>
-    FAQ
-  </li>
-  <li className='group relative cursor-pointer px-4 py-1 rounded-xl underline underline-offset-8 hover:no-underline'>
-    <span className='absolute inset-x-0 top-0 h-0.5 bg-white transform scale-x-0 transition-transform duration-300 ease-out group-hover:scale-x-100'></span>
-    Contact
-  </li>
-</ul>
-
-        {/* College Logo */}
-        <div className="hidden md:col-span-3 md:flex justify-end items-center">
-          <img className="w-56" src={collegeLogo} alt="" />
         </div>
-      </div>
-    </div>
   );
 };
 
