e89f9c4fc74d3cd66ba40653c329cf827a02e5b5
Jefin Rojar <jefinrojar@gmail.com>
2025-02-27 11:35:48 +0530

    Events page responsiveness completed

--- Frontend/src/Pages/Events/Events.jsx
+++ Frontend/src/Pages/Events/Events.jsx
@@ -4,15 +4,16 @@ import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import { useNavigate } from "react-router-dom";
-import TechnicalImage from "../../assets/EventsImages/technical.png"; 
-import NonTechnicalImage from "../../assets/EventsImages/non-technical.png"
-import Cultural from "../../assets/EventsImages/culturals.jpg"
-import GuestLecture from "../../assets/EventsImages/guestLecture.jpg"
-import Workshop from "../../assets/EventsImages/workshop.jpg"
+import TechnicalImage from "../../assets/EventsImages/technical.png";
+import NonTechnicalImage from "../../assets/EventsImages/non-technical.png";
+import Cultural from "../../assets/EventsImages/culturals.jpg";
+import Workshop from "../../assets/EventsImages/workshop.jpg";
 
 const Events = () => {
   const [selectedEvent, setSelectedEvent] = useState(1);
   const [rotation, setRotation] = useState(0);
+  const [isHovered, setIsHovered] = useState(false); // State to track hover for slider
+  const [hoveredCircle, setHoveredCircle] = useState(null); // State to track hover for circles
   const navigate = useNavigate();
 
   const events = [
@@ -38,7 +39,7 @@ const Events = () => {
     {
       id: 2,
       image: NonTechnicalImage,
-      name: "Technical Events",
+      name: "Non-Technical Events",
       descriptionImages: [
         {
           image: "https://images.unsplash.com/photo-1739609579483-00b49437cc45?q=80&w=2071&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
@@ -73,25 +74,6 @@ const Events = () => {
         },
       ],
     },
-    // {
-    //   id: 4,
-    //   image: GuestLecture,
-    //   name: "GuestLecture",
-    //   descriptionImages: [
-    //     {
-    //       image: "https://images.unsplash.com/photo-1739609579483-00b49437cc45?q=80&w=2071&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-    //       eventId: "guest-lecture-1",
-    //     },
-    //     {
-    //       image: "https://images.unsplash.com/photo-1718563552473-2d97b224e801?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-    //       eventId: "guest-lecture-2",
-    //     },
-    //     {
-    //       image: "https://images.unsplash.com/photo-1732491482042-bfb6bc27678e?q=80&w=2053&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-    //       eventId: "guest-lecture-3",
-    //     },
-    //   ],
-    // },
     {
       id: 5,
       image: Workshop,
@@ -139,26 +121,46 @@ const Events = () => {
   });
 
   const handleSlideClick = (eventId) => {
-    console.log("Clicked eventId:", eventId); // Debug: Log the eventId
-    console.log("Navigating to:", `/event/${eventId}`); // Debug: Log the URL
-    navigate(`/event/${eventId}`); // Navigate to the dynamic route
+    navigate(`/event/${eventId}`);
   };
 
-  // Settings for react-slick
+  // Responsive settings for react-slick
   const sliderSettings = {
-    dots: true, // Show dots for navigation
-    infinite: true, // Infinite loop
-    speed: 500, // Transition speed
-    slidesToShow: 1, // Number of slides to show at once
-    slidesToScroll: 1, // Number of slides to scroll
-    autoplay: true, // Enable autoplay
-    autoplaySpeed: 2000, // Autoplay interval in milliseconds
-    pauseOnHover: false, // Disable pause on hover
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    pauseOnHover: false,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          dots: false,
+        },
+      },
+    ],
   };
 
   return (
-    <motion.div initial={{ opacity: 0, y: 20 }} animate={{ opacity: 1, y: 0 }} transition={{ duration: 1, ease: "easeInOut" }}>
-      <h1 className="font-bold w-full text-center mt-32 space-x-2 text-4xl text-white">
+    <motion.div
+      initial={{ opacity: 0, y: 20 }}
+      animate={{ opacity: 1, y: 0 }}
+      transition={{ duration: 1, ease: "easeInOut" }}
+      className="w-full"
+    >
+      <h1 className="font-bold w-full text-center mt-32 md:mt-16 lg:mt-32 space-x-2 text-2xl sm:text-3xl md:text-4xl text-white">
         {Array.from("Events").map((letter, index) => (
           <motion.span key={index} initial={{ y: 0 }} animate={letterAnimation(index)} style={{ display: "inline-block" }}>
             {letter}
@@ -166,16 +168,17 @@ const Events = () => {
         ))}
       </h1>
 
-      <div className="w-full flex flex-row py-3 relative px-10">
-        <div className="w-2/3 flex flex-col items-start py-10 relative pl-20">
+      <div className="w-full flex flex-col lg:flex-row py-3 relative px-4 sm:px-6 md:px-10">
+        {/* Circular Event Selector */}
+        <div className="w-full lg:w-2/3 md:w-2/3 flex flex-col items-center lg:items-start py-6 lg:py-10 relative lg:pl-20">
           <motion.div
-            className="relative w-[600px] h-[600px] flex items-center justify-center rounded-full"
+            className="relative w-[200px] h-[200px] sm:w-[300px] sm:h-[300px] md:w-[400px] md:h-[400px] lg:w-[600px] lg:h-[600px] flex items-center justify-center rounded-full"
             animate={{ rotate: rotation }}
             transition={{ type: "spring", stiffness: 100, damping: 15, mass: 1 }}
           >
             {events.map((event, index) => {
               const angle = (index / events.length) * 2 * Math.PI;
-              const radius = 240;
+              const radius = window.innerWidth < 1024 ? (window.innerWidth < 768 ? 80 : 120) : 240;
               const x = Math.cos(angle) * radius;
               const y = Math.sin(angle) * radius;
               const isActive = selectedEvent === event.id;
@@ -183,20 +186,35 @@ const Events = () => {
               return (
                 <motion.div
                   key={event.id}
-                  className={`absolute flex items-center justify-center w-52 h-52 rounded-full shadow-lg cursor-pointer transition-all duration-500 overflow-hidden border-4 border-white ${
-                    isActive ? "bg-yellow-500 scale-125 shadow-glow" : "bg-blue-500"
-                  } hover:w-48 hover:h-48`}
+                  className={`absolute flex items-center justify-center w-20 h-20 sm:w-28 sm:h-28 md:w-32 md:h-32 lg:w-52 lg:h-52 rounded-full shadow-lg cursor-pointer transition-all duration-500 overflow-hidden border-4 border-white ${isActive ? "bg-yellow-500 scale-125 shadow-glow" : "bg-blue-500"
+                    } hover:w-16 hover:h-16 sm:hover:w-24 sm:hover:h-24 md:hover:w-28 md:hover:h-28 lg:hover:w-48 lg:hover:h-48`}
                   style={{ transform: `translate(${x}px, ${y}px)` }}
                   onClick={() => handleEventClick(event.id, index)}
+                  onMouseEnter={() => setHoveredCircle(event.id)} // Set hover state to true
+                  onMouseLeave={() => setHoveredCircle(null)} // Set hover state to false
                 >
                   <img src={event.image} alt={event.name} className="w-full h-full object-cover" />
+                  {/* Backdrop and Event Name */}
+                  {hoveredCircle === event.id && (
+                    <div className="absolute inset-0 bg-black bg-opacity-50 flex items-center justify-center">
+                      <motion.span
+                        className="text-white text-[10px] lg:text-xl md:text-2xl font-bold text-center"
+                        initial={{ opacity: 0 }}
+                        animate={{ opacity: 1 }}
+                        transition={{ duration: 0.5 }}
+                      >
+                        {event.name}
+                      </motion.span>
+                    </div>
+                  )}
                 </motion.div>
               );
             })}
           </motion.div>
         </div>
 
-        <div className="w-1/2 flex items-center justify-center p-10">
+        {/* Event Details Slider */}
+        <div className="w-full lg:w-1/2 flex items-center justify-center p-4 sm:p-6 md:p-10">
           <motion.div
             key={selectedEventData?.name}
             className="w-full h-auto shadow-2xl border border-sky-800 p-3 bg-gradient-to-b text-sky-500 transition-all duration-500"
@@ -204,23 +222,37 @@ const Events = () => {
             animate={{ opacity: 1, y: 0 }}
           >
             <div className="bg-sky-900 bg-opacity-30">
-              <h2 className="text-3xl font-semibold mb-4 py-3 border-b border-sky-800 mx-2 text-center">
+              <h2 className="text-xl sm:text-2xl md:text-3xl font-semibold mb-4 py-3 border-b border-sky-800 mx-2 text-center">
                 {selectedEventData ? selectedEventData.name : "Event Details"}
               </h2>
 
-              {/* React Slick Slider */}
               <Slider {...sliderSettings}>
                 {selectedEventData?.descriptionImages.map((slide, index) => (
                   <div
                     key={`slide-${slide.eventId}-${index}`}
-                    className="flex flex-col items-center justify-center p-4 rounded-xl shadow-lg h-[450px] cursor-pointer"
-                    onClick={() => handleSlideClick(slide.eventId)} // Pass eventId directly
+                    className="relative flex flex-col items-center justify-center p-4 rounded-xl shadow-lg h-[200px] sm:h-[300px] md:h-[400px] lg:h-[450px] cursor-pointer"
+                    onClick={() => handleSlideClick(slide.eventId)}
+                    onMouseEnter={() => setIsHovered(true)} // Set hover state to true
+                    onMouseLeave={() => setIsHovered(false)} // Set hover state to false
                   >
                     <img
                       src={slide.image}
                       alt={`Slide ${index + 1}`}
                       className="w-full h-full object-cover shadow-md clip-bottom-left"
                     />
+                    {/* Backdrop and Text */}
+                    {isHovered && (
+                      <div className="absolute inset-0 bg-black bg-opacity-50 flex items-center justify-center">
+                        <motion.span
+                          className="text-white text-2xl sm:text-3xl md:text-4xl font-bold"
+                          initial={{ opacity: 0 }}
+                          animate={{ opacity: 1 }}
+                          transition={{ duration: 0.5 }}
+                        >
+                          Click me to register for the event
+                        </motion.span>
+                      </div>
+                    )}
                   </div>
                 ))}
               </Slider>
