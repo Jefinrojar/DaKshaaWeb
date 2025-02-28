bfdf6d46991407b77f8feccd9ed721876c8edcfb
Gobihajs <mailtogobiha@gmail.com>
2025-02-28 15:50:14 +0530

    harmoics_pic

--- Frontend/src/Pages/Harmonics/Components/HarmonicsSection.jsx
+++ Frontend/src/Pages/Harmonics/Components/HarmonicsSection.jsx
@@ -1,12 +1,8 @@
 import React from 'react';
-import Photo1 from "../../../assets/photo1.jpg";
+import Photo1 from "../../../assets/photo1.png";
 import Photo2 from "../../../assets/photo2.jpg";
 import Photo3 from "../../../assets/photo3.jpg";
 import Photo4 from "../../../assets/photo4.jpg";
-import Photo5 from "../../../assets/photo5.jpg";
-import Photo6 from "../../../assets/photo6.jpg";
-import Photo7 from "../../../assets/photo7.jpg";
-import Photo8 from "../../../assets/photo8.jpg";
 
 import { motion } from 'framer-motion';
 
