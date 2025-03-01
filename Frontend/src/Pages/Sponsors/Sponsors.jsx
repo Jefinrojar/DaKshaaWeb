0932ef4cac3cf478bbd01b5ed2e22231fe2c5bb5
Praveen2365 <praveenboy2306@gmail.com>
2025-03-01 14:55:09 +0530

    changes

--- Frontend/src/Pages/Sponsors/Sponsors.jsx
+++ Frontend/src/Pages/Sponsors/Sponsors.jsx
@@ -17,9 +17,13 @@ import vlsi from "./assets/vlsi.jpg";
 import cse from "./assets/cse.jpg";
 import bt from "./assets/bt.jpeg";
 
+
+
 function Sponsors() {
-  const eventSponsors = [abb, unstop];
+  const eventSponsors = [abb];
+  const registerSponsors = [unstop]; 
   const workshopSponsors = [aiml, civil, csbs, it, cse, mech, txt, millet, infinity, garuda, uipath, vlsi, bt];
+  // Add more if needed
   const letter = "Sponsors";
   const letters = letter.split("");
 
@@ -63,7 +67,7 @@ function Sponsors() {
       {/* Event Partner Section */}
       <div className="text-center w-full">
         <h2 className="text-3xl font-bold mb-10">Event Partner</h2>
-        <div className="flex justify-center items-center gap-24 ">
+        <div className="flex justify-center items-center gap-24">
           {eventSponsors.map((image, index) => (
             <motion.div
               key={index}
@@ -73,6 +77,24 @@ function Sponsors() {
               className="shadow-lg flex justify-center items-center bg-gray-800 w-48 h-48 rounded-lg border-2 border-sky-500 shadow-sky-500"
             >
               <img className="w-40 h-40 object-contain rounded-lg" src={image} alt={`Event Partner ${index + 1}`} />
+            </motion.div>
+          ))}
+        </div>
+      </div>
+
+      {/* Register Partner Section */}
+      <div className="text-center w-full">
+        <h2 className="text-3xl font-bold m-10">Registration Partner</h2>
+        <div className="flex justify-center items-center gap-24">
+          {registerSponsors.map((image, index) => (
+            <motion.div
+              key={index}
+              initial={{ opacity: 0, scale: 0.8, rotateY: 180 }}
+              animate={cardEntryAnimation(index)}
+              whileHover={{ scale: 1.15, rotateY: 5 }}
+              className="shadow-lg flex justify-center items-center bg-gray-800 w-48 h-48 rounded-lg border-2 border-sky-500 shadow-sky-500"
+            >
+              <img className="w-40 h-40 object-contain rounded-lg" src={image} alt={`Register Partner ${index + 1}`} />
             </motion.div>
           ))}
         </div>
